47859f7a45e956be09f42962bc44acef0d5e2120
janutaru <janutaru143@gmail.com>
2024-11-29 13:53:49 +0530

    Update main.tf

--- modules/eks-cluster/main.tf
+++ modules/eks-cluster/main.tf
@@ -1,1 +1,25 @@
+resource "aws_eks_cluster" "main" {
+  name     = var.cluster_name
+  role_arn = var.cluster_role_arn
 
+  vpc_config {
+    subnet_ids          = var.subnet_ids
+    security_group_ids  = [var.security_group_id]
+    endpoint_private_access = true
+    endpoint_public_access = true
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster_policy_attachment]
+}
+
+output "cluster_name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "cluster_endpoint" {
+  value = aws_eks_cluster.main.endpoint
+}
+
+output "cluster_certificate_authority" {
+  value = aws_eks_cluster.main.certificate_authority[0].data
+}
