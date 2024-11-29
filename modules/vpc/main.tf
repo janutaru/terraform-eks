2be61ffa647371a04f53cc7f21169bad7692f8c6
janutaru <janutaru143@gmail.com>
2024-11-29 13:52:17 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,1 +1,32 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
+  enable_dns_support = true
+  enable_dns_hostnames = true
+}
 
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_a_cidr
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_b_cidr
+  availability_zone       = "us-west-2b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "eks_sg" {
+  name_prefix = "eks_sg_"
+  vpc_id      = aws_vpc.main.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_ids" {
+  value = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
+}
