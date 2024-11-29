eed5104057d5806435d66b633c1d32e5bc92be65
janutaru <janutaru143@gmail.com>
2024-11-29 13:51:30 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -1,1 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket"
+    key            = "eks-cluster/terraform.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
 
+provider "aws" {
+  region = "us-west-2"
+}
