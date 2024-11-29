48f24095136ae20972eaa3ed222dd12dcf2e9db8
janutaru <janutaru143@gmail.com>
2024-11-29 13:59:49 +0530

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -11,3 +11,34 @@ terraform {
 provider "aws" {
   region = "us-west-2"
 }
+
+module "vpc" {
+  source      = "./modules/vpc"
+  cidr_block  = "10.0.0.0/16"
+  subnet_a_cidr = "10.0.1.0/24"
+  subnet_b_cidr = "10.0.2.0/24"
+}
+
+module "iam" {
+  source = "./modules/iam"
+}
+
+module "eks_cluster" {
+  source            = "./modules/eks-cluster"
+  cluster_name      = "my-eks-cluster"
+  cluster_role_arn  = module.iam.eks_cluster_role_arn
+  subnet_ids        = module.vpc.subnet_ids
+  security_group_id = module.vpc.eks_sg.id
+}
+
+output "eks_cluster_name" {
+  value = module.eks_cluster.cluster_name
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks_cluster.cluster_endpoint
+}
+
+output "eks_cluster_certificate_authority" {
+  value = module.eks_cluster.cluster_certificate_authority
+}
