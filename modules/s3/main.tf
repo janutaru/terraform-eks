d15426e1503cc5374832c44498f8e7364904b316
janutaru <janutaru143@gmail.com>
2024-12-02 11:19:16 +0530

    Create main.tf

---
+++ modules/s3/main.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "my-terraform-state-bucket" 
+}
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
